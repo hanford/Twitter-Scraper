5cf4e830f147baee079ec126e6222a408a7224a6
Jack Hanford <jackhanford@gmail.com>
2014-10-12 19:32:32 -0700

    implementing search as service to be called multiple times when line breaks are detected

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,36 +1,47 @@
 'use strict';
 
-/* Controllers */
+angular.module('myApp.controllers', ['myApp.services'])
 
-angular.module('myApp.controllers', []).
-  controller('AppCtrl', function ($scope, $http) {
-    console.log('loaded')
+.controller('AppCtrl', function($scope, $http) {
+  console.log('loaded')
 
-  }).controller('MyCtrl1', function ($scope, $http) {
-    $scope.users = [];
+}).controller('MyCtrl1', function($scope, $http, apiService) {
+  $scope.users = [];
 
-    $scope.search = function(query) {
-      $scope.loading = true;
-      $http.get('/api/search/users?query=' + query).then(function(response) {
+  $scope.search = function(query) {
+    $scope.loading = true;
+    $scope.names = query.split(/\r\n|\r|\n/g)
+    console.log($scope.names.length > 1)
+    if ($scope.names.length > 1) {
+      setTimeout(function() {
+        for (var i = 0; i < $scope.names.length; i++) {
+          console.log($scope.names[i])
+          apiService.search($scope.names[i]).then(function(response) {
+            console.log(response)
+          })
+        }
+      }, 1000)
+      $scope.loading = false;
+    } else {
+      apiService.search(query).then(function(response) {
+        console.log(response)
         $scope.loading = false;
         $scope.users = response.data.users;
       })
     }
-    $scope.hashtag = function(query) {
-      console.log(encodeURI(query));
-      $scope.loading = true;
-      $http.get('/api/search/tweets?query=' + encodeURI(query)).then(function(response) {
-        $scope.loading = false;
-        if (response.data && response.data.tweets && response.data.tweets.statuses) {
-          $scope.tweets = response.data.tweets.statuses;
-          $scope.nothing = false;
-        } else {
-          $scope.nothing = true;
-        }
-      })
-    }
-    $scope.clear = function() {
-      $scope.tweets = [];
-      $scope.users = [];
-    }
-  })
+  }
+  $scope.hashtag = function(query) {
+    console.log(encodeURI(query));
+    $scope.loading = true;
+    $http.get('/api/search/tweets?query=' + encodeURI(query)).then(function(response) {
+      $scope.loading = false;
+      if (response.data && response.data.tweets && response.data.tweets.statuses) {
+        $scope.tweets = response.data.tweets.statuses;
+      }
+    })
+  }
+  $scope.clear = function() {
+    $scope.tweets = [];
+    $scope.users = [];
+  }
+})
--- public/js/services.js
+++ public/js/services.js
@@ -1,9 +1,11 @@
 'use strict';
 
-/* Services */
+angular.module('myApp.services', [])
 
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+.service('apiService', ['$http', function($http) {
+  return {
+    search: function(query) {
+      return $http.get('/api/search/users?query=' + query)
+    }
+  }
+}])
